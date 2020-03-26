810b7b02011c38b4dd469678b8c220fced06eb84
da-moon <damoon.azarpazhooh@ryerson.ca>
2020-03-26 02:13:07 +0000

    get-go #6

--- cmd/get-vscode/get-vscode.sh
+++ cmd/get-vscode/get-vscode.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 # shellcheck source=./lib/install/init.sh
 source "$(cd "$(dirname "$(dirname "$(dirname "${BASH_SOURCE[0]}")")")" && pwd)/lib/install/init.sh"
-# shellcheck source=./lib/install/java.sh
-source "$(cd "$(dirname "$(dirname "$(dirname "${BASH_SOURCE[0]}")")")" && pwd)/lib/install/java.sh"
+# shellcheck source=./lib/install/vscode.sh
+source "$(cd "$(dirname "$(dirname "$(dirname "${BASH_SOURCE[0]}")")")" && pwd)/lib/install/vscode.sh"
 
 if [ -z "${BASH_SOURCE+x}" ]; then
     init
-    java_installer
+    vscode_installer
+    vscode_extension_installer
     exit $?
 else
     if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
         init
-        java_installer
+        vscode_installer
+        vscode_extension_installer
         exit $?
     fi
 fi
